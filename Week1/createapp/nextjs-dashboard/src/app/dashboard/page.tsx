f8a943ce7fa57c65f1fd18606c54c867f089de0c
lamhai <hoangdinhlamhai1@gmail.com>
2025-12-09 21:54:59 +0700

    Navigating Between Pages

--- Week1/createapp/nextjs-dashboard/src/app/dashboard/page.tsx
+++ Week1/createapp/nextjs-dashboard/src/app/dashboard/page.tsx
@@ -1,4 +1,21 @@
 //http://localhost:3000/dashboard
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 export default function Page() {
-  return <p>Dashboard Page</p>;
+    const pathname = usePathname(); // Get the current path
+  return (
+    <>
+        <p>{pathname}</p>
+        <p>Dashboard Page</p>
+        <Link href="/dashboard/customers">
+          Customer
+        </Link>
+        <Link href="/dashboard/invoices">
+          Invoices
+        </Link>
+    </>
+  );
 }
