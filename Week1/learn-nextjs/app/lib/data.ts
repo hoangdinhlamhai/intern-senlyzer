c18939d94f05f70200f3a8bb49259571cac2270b
lamhai <hoangdinhlamhai1@gmail.com>
2025-12-14 19:56:53 +0700

    update

--- Week1/learn-nextjs/app/lib/data.ts
+++ Week1/learn-nextjs/app/lib/data.ts
@@ -142,6 +142,31 @@ export async function fetchInvoicesPages(query: string) {
   }
 }
 
+export async function fetchInvoiceById(id: string) {
+  try {
+    const data = await sql<InvoiceForm[]>`
+      SELECT
+        invoices.id,
+        invoices.customer_id,
+        invoices.amount,
+        invoices.status
+      FROM invoices
+      WHERE invoices.id = ${id};
+    `;
+
+    const invoice = data.map((invoice) => ({
+      ...invoice,
+      // Convert amount from cents to dollars
+      amount: invoice.amount / 100,
+    }));
+
+    return invoice[0];
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch invoice.');
+  }
+}
+
 export async function fetchCustomers() {
   try {
     const customers = await sql<CustomerField[]>`
