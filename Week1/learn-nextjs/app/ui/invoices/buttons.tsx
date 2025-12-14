0bb6fd74c65409c9479ac6701acc22b41b0146b6
lamhai <hoangdinhlamhai1@gmail.com>
2025-12-14 20:16:29 +0700

    fix url

--- Week1/learn-nextjs/app/ui/invoices/buttons.tsx
+++ Week1/learn-nextjs/app/ui/invoices/buttons.tsx
@@ -17,7 +17,7 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/ui/invoices/${id}/edit"
+      href={`/ui/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
