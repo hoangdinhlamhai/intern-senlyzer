de3a750a66464875808888c88981a57e9009588f
lamhai <hoangdinhlamhai1@gmail.com>
2025-12-15 11:16:23 +0700

    fix export default

--- Week1/seo/nextjs-lighthouse/app/posts/[slug]/page.tsx
+++ Week1/seo/nextjs-lighthouse/app/posts/[slug]/page.tsx
@@ -1,20 +1,32 @@
-import { Metadata } from 'next';
+import type { Metadata } from "next";
 
 type Props = {
   params: { slug: string };
 };
 
-export default async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const postUrl = `https://my-app.com/posts/${params.slug}`;
+//metadata: KHÔNG default
+export async function generateMetadata(
+  { params }: Props
+): Promise<Metadata> {
+  const postUrl = `https://my-app.com/posts/${params.slug}`;
 
-    //url gọi api chụp ảnh
-      const ogImageUrl = `https://my-app.com/api/og-screenshot?url=${encodeURIComponent(postUrl)}`;
+  const ogImageUrl = `https://my-app.com/api/og-screenshot?url=${encodeURIComponent(
+    postUrl
+  )}`;
 
-    return {
-        title: `Bài viết ${params.slug}`,
-        openGraph: {
-        images: [ogImageUrl], // Đây chính là output từ API chụp ảnh
-        },
-    };
+  return {
+    title: `Bài viết ${params.slug}`,
+    openGraph: {
+      images: [ogImageUrl],
+    },
+  };
+}
 
+// page:default export
+export default function Page({ params }: Props) {
+  return (
+    <main>
+      <h1>Bài viết {params.slug}</h1>
+    </main>
+  );
 }
